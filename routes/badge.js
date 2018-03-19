51d9905198ee18a03be00827fe9255dd47962495
Julius Tens <mail@juliustens.eu>
2018-03-19 21:10:52 +0100

    return grey badge instead of error message on error

--- routes/badge.js
+++ routes/badge.js
@@ -22,13 +22,12 @@ const route = async (req, res, next) => {
 	const path = req.path.slice('/badge/'.length) // todo
 	const repo = githubInfo.fromUrl(path)
 	const fptf = await (getVersion(repo).catch(e => {
-		next(error(e, 400))
+		// next(error(e, 400))
 		return null
 	}))
-	if(!fptf) return
-
-    const b = await (badge(fptf, '#ff66bb').catch(e => null))
-	// const b = await (badge('invalid', '#9f9f9f').catch(e => null))
+	let b
+	if(!fptf) b = await (badge('invalid', '#9f9f9f').catch(e => null))
+	else b = await (badge(fptf, '#ff66bb').catch(e => null))
     if(!b) return next(error('error while generating badge', 500))
 
     res.setHeader('Content-Type', 'image/svg+xml')
